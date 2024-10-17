62c44a82ce2c4bd57601d6ff89017d02c798a665
AzwarKusumah <azwarmeizia.kusumah@gmail.com>
2024-10-17 09:43:16 +0700

    Feat: add repository

--- install.sh
+++ install.sh
@@ -26,7 +26,25 @@ echo "Checking OS..."
 detect_os
 
 install_proxmox_debian(){
-    echo "This debian"
+    if [ "$VERSION" = "12" ]; then
+        echo "Ryuko: Adding Proxmox VE Sourcelist..."
+        echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+
+        echo "Ryuko: Adding Proxmox VE repository Key..."
+        wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
+
+        echo "Ryuko: Verify your key..."
+        sha512sum /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg 
+
+        echo "Ryuko: Updating and Upgrading your system..."
+        apt update && apt full-upgrade-y
+        
+    elif [ "$VERSION" = "11" ]; then
+
+    else
+        echo "There is no version"
+        exit
+    fi
 }
 
 install_proxmox_ubuntu(){
@@ -34,9 +52,9 @@ install_proxmox_ubuntu(){
 }
 
 install_proxmox_on(){
-    if ["$OS" == "debian"]; then
+    if [ "$OS" = "debian" ]; then
         install_proxmox_debian
-    elif ["$OS" == "bbuntu"]; then
+    elif [ "$OS" = "ubuntu" ]; then
         install_proxmox_ubuntu
     else 
         echo "This script only support Debian and Ubuntu"
